418df87248b9fa4b8c814bcb0f39954e5c101cab
Sathya <neelasathya06@gmail.com>
2025-05-30 23:54:43 +0530

    Product Layout cemmented

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {FaBeer} from "react-icons/fa";
+import Products from "./components/Products.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div className='flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold'>
-       <h1>Welcome </h1> <FaBeer/>
+   <div >
+       <Products />
    </div>
   )
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,55 @@
+import ProductCard from "./ProductCard.jsx";
+import {FaExclamationCircle, FaExclamationTriangle} from "react-icons/fa";
+
+const Products = () => {
+    const isLoading = false;
+    const errorMessage = "Error loading products.";
+    const products = [
+        {
+            productId: 652,
+            productName: "IPhone Xs max",
+            image: "https://placehold.co/600x400",
+            description: "Experience the latest in mobile technology with advertisement",
+            quantity: 0,
+            price: 1450.0,
+            discount: 10.0,
+            specialPrice: 1305.0,
+        },
+        {
+            productId: 654,
+            productName: "Macbook Air M25",
+            image: "https://placehold.co/600x400",
+            description: "Ultra thin laptop with Apple's M2 Chip, providing new experience",
+            quantity: 0,
+            price: 2550.0,
+            discount: 15.0,
+            specialPrice: 2305.0,
+        },
+    ]
+
+    return (
+        <div className='lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto'>
+            {isLoading ? (
+                <p>Loading...</p>
+            ): errorMessage ? (
+                <div className="flex justify-center items-center h-[200px]">
+                    <FaExclamationTriangle className="text-slate-800 text-3xl mr-2"  />
+                    <span className="text-slate-800 text-lg font-medium">
+                        {errorMessage}
+                    </span>
+                </div>
+            ):(
+                <div className="min-h-[700px]">
+                    <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+                        {products &&
+                        products.map((item, i) =>
+                            <ProductCard  key={i} {...item} />
+                        )}
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Products
