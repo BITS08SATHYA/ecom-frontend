c74cbdc1c546dff2bbe34e68aac0c9d3b37fcc54
Sathya <neelasathya06@gmail.com>
2025-06-04 00:42:08 +0530

    Product Layout cemmented

--- src/components/ProductViewModal.jsx
+++ src/components/ProductViewModal.jsx
@@ -2,6 +2,7 @@ import {Button, Dialog, DialogBackdrop, DialogPanel, DialogTitle} from '@headles
 import { useState } from 'react'
 import {MdClose, MdDone} from "react-icons/md";
 import Status from "./Status.jsx";
+import {Divider} from "@mui/material";
 
 function ProductViewModal({open, setOpen, product, isAvailable}) {
 
@@ -56,41 +57,68 @@ function ProductViewModal({open, setOpen, product, isAvailable}) {
                                 )
                             }
                             <div className='px-6 pt-10 pb-2'>
-                                <DialogTitle as="h1" className="lg:text-3xl sm:text-2xl text-xl font-semibold leading-6  text-gray-800 mb-4">
+                                <DialogTitle as="h1" className="lg:text-3xl sm:text-2xl text-xl font-semibold leading-6 text-gray-800 mb-4">
                                     {productName}
                                 </DialogTitle>
                             </div>
                             <div>
-                            {
-                                isAvailable ? (
-                                <Status
-                                    text="In Stock"
-                                    icon={MdDone}
-                                    bg="bg-teal-200"
-                                    color="text-teal-900"
-                                />
-                                ) : (
-                                <Status
-                                    text="Out of Stock"
-                                    icon={MdClose}
-                                    bg="bg-rose-200"
-                                    color="text-rose-905"
-                                />
-                                )
-                            }
-                    </div>
 
-                            <p className="mt-2 text-sm/6 text-slate-600">
-                                {description}
-                            </p>
-                            <div className="mt-4">
-                                <Button
-                                    className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
-                                    onClick={() => setOpen(false)}
-                                >
-                                    Got it, thanks!
-                                </Button>
+
+
+                            <div className="space-y-2 text-gray-700 pb-4">
+                                <div className="pl-6 pr-6 flex items-center justify-between gap-2 ">
+                                    {
+                                        specialPrice ? (
+                                            <div className="flex items-center gap-2">
+                                                <span className="text-gray-400 line-through">
+                                                    ${Number(price).toFixed(2)}
+                                                </span>
+                                                <span className="sm:text-xl font-semibold text-slate-700">
+                                                    ${Number(specialPrice).toFixed(2)}
+                                                </span>
+                                            </div>
+                                        ):(
+                                            <span className="text-xl font-bold">
+                                                {" "}
+                                                ${Number(price).toFixed(2)}
+                                            </span>
+                                        )
+                                    }
+
+                                    {
+                                        isAvailable ? (
+                                            <Status
+                                                text="In Stock"
+                                                icon={MdDone}
+                                                bg="bg-teal-200"
+                                                color="text-teal-900"
+                                            />
+                                        ) : (
+                                            <Status
+                                                text="Out of Stock"
+                                                icon={MdClose}
+                                                bg="bg-rose-200"
+                                                color="text-rose-905"
+                                            />
+                                        )
+                                    }
+                                </div>
+
+                                <Divider />
+                                <p className="pt-1 ml-6 ">{description}</p>
+                                </div>
                             </div>
+
+                            <div className="px-6 py-4 flex justify-end gap-4">
+                                <button onClick={() => setOpen(false)}
+                                        type="button"
+                                        className="px-4 py-2 text-sm font-semibold text-slate-700 border hover:text-slate-800 hover:border-slate-800 rounded-md cursor-pointer"
+                                >
+                                    Close
+                                </button>
+                            </div>
+
+
                         </DialogPanel>
                     </div>
                 </div>
