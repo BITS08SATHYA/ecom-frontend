3caca2bc633752640e80c391e630ac9c3117ae09
Sathya <neelasathya06@gmail.com>
2025-06-08 23:59:44 +0530

    setting up banner on home page with corrections pending!

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                customBlue: "rgba(28,100,242,1)",
+                banner: {
+                    color1: "#FDC200",
+                    color2: "#FF2C2C",
+                    color3: "#21AD61",
+                    color4: "#723DA6",
+                }
+            }
+        },
     },
     plugins: [],
 };
