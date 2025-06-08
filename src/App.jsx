99702a8e3bb3ff478ee5ecc028d110e69e0354c3
Sathya <neelasathya06@gmail.com>
2025-06-08 23:09:56 +0530

    Routing impl!

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {FaBeer} from "react-icons/fa";
 import Products from "./components/products/Products.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div >
-       <Products />
-   </div>
+   <Router>
+       <Routes>
+           <Route path='/' element={ <Home /> } />
+           <Route path='/products' element={ <Products /> } />
+       </Routes>
+   </Router>
   )
 }
 
