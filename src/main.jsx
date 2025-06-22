48a3589a38f729650b0b674dbede6e98071517dd
sathya <neelasathya06@gmail.com>
2025-06-22 23:30:21 +0530

    getting rid of the strict mode impl

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import store from "./store/reducers/Store.js";
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-      <StrictMode>
+
 
             <App />
 
-      </StrictMode>
+
     </Provider>,
 )
