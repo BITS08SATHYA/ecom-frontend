b00c4414a544403404c99acfca1628e6bd829238
sathya <neelasathya06@gmail.com>
2025-06-22 19:55:44 +0530

    Address Selection: completed the address flow impl

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,17 +1,40 @@
 import React, { useState, useEffect} from 'react';
-import {Step, StepLabel, Stepper} from "@mui/material";
+import {Button, Step, StepLabel, Stepper} from "@mui/material";
 import AddressInfo from "./AddressInfo.jsx";
 import {useDispatch, useSelector} from "react-redux";
 import {getUserAddresses} from "../../store/actions/index.js";
+import toast from "react-hot-toast";
 
 const Checkout = () => {
     const dispatch = useDispatch();
     const [activeStep, setActiveStep] = useState(0);
     const { isLoading, errorMessage } = useSelector((state) => state.errors);
+    const { address, selectedUserCheckoutAddress } = useSelector((state) => state.auth)
+    // const { address }  = useSelector(
+    //     (state) => state.auth
+    // )
 
-    const { address }  = useSelector(
-        (state) => state.auth
-    )
+    const paymentMethod = false;
+
+
+    const handleBack = () => {
+        setActiveStep((prevStep) => prevStep - 1);
+    }
+
+    const handleNext = () => {
+
+        if(activeStep === 0 && !selectedUserCheckoutAddress) {
+            toast.error("Please select checkout address before procedding.")
+            return
+        }
+
+        if(activeStep === 1 && (!selectedUserCheckoutAddress || !paymentMethod)) {
+            toast.error("Please select payment address before procedding.")
+            return
+        }
+
+        setActiveStep((prevStep) => prevStep + 1);
+    }
 
     const steps = [
         "Address",
@@ -39,6 +62,40 @@ const Checkout = () => {
                     activeStep === 0 && <AddressInfo address={address} />
                 }
             </div>
+
+            <div
+                style={{boxShadow: "0 -2px 4px rgba(100,100,100,0.15"}}
+                className="flex justify-between items-center px-4 fixed z-50 h-24 bottom-0 bg-white left-0 w-full py-4 border-slate-800">
+                <Button
+                    variant='outlined'
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    >
+                        Back
+                </Button>
+                {activeStep !== steps.length -1 && (
+                    <button
+                        className={`bg-blue-600 font-semibold px-6 h-10 rounded-md text-white ${
+                            errorMessage ||
+                            (activeStep === 0 && !selectedUserCheckoutAddress) ||
+                            (activeStep === 1 && !paymentMethod)
+                            ? "opacity-60"
+                                : ""
+                        }`}
+                        disabled={
+                            errorMessage || (
+                                activeStep === 0 ? !selectedUserCheckoutAddress
+                                    : activeStep === 1 ? !paymentMethod : false
+                            )
+                        }
+                        onClick={handleNext}
+                    >
+                        Proceed
+                    </button>
+                )}
+            </div>
+
+
         </div>
     )
 }
