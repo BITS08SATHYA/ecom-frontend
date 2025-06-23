0f11929c77892fc45921b12f4ee894d7aabe8cc4
sathya <neelasathya06@gmail.com>
2025-06-23 22:15:48 +0530

    Stripe Integration Implemented

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -242,6 +242,7 @@ export const clearCheckoutAddress = () => {
 }
 
 export const selectUserCheckoutAddress = (address) => {
+    localStorage.setItem("CHECKOUT_ADDRESS", JSON.stringify(address));
     return {
         type: "SELECT_CHECKOUT_ADDRESS",
         payload: address
@@ -306,5 +307,31 @@ export const createStripePaymentSecret
     }catch(error){
         console.log(error)
         toast.error(error.response?.data?.message  || "Failed to create client secret");
+    }
+}
+
+export const stripePaymentConfirmation
+    = ( sendData ,setErrorMessage, setLoading, toast ) => async (dispatch, getState) => {
+    try{
+
+        // console.log("Send Data check: " , sendData)
+
+        const  response  = await api.post("/order/users/payments/online",
+            sendData,
+        );
+
+        if(response.data){
+            localStorage.removeItem("CHECKOUT_ADDRESS");
+            localStorage.removeItem("cartItems");
+            localStorage.removeItem("client-secret")
+            dispatch({ type: "REMOVE_CLIENT_SECRET_ADDRESS" });
+            dispatch({ type: "CLEAR_CART" })
+            toast.success("Order Accepted")
+        }else{
+            setErrorMessage("Payment Failed, Please try again!")
+        }
+    }catch(error){
+        setErrorMessage("Payment Failed, Please try again!")
+        // toast.error("Payment Failed, Please try again!")
     }
 }
